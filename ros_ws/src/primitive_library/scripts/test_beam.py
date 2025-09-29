bf85b2742aa830766c567f4cd16104fe7676723e
mourawas <mouhamad.rawas@epfl.ch>
2025-09-29 13:21:51 +0200

    simpler experiment script, not yet working

---
+++ ros_ws/src/primitive_library/scripts/test_beam.py
@@ -0,0 +1,35 @@
+import rospy
+import time
+from primitives.action_functions import approach, pick, place, drop
+from primitives.predicates import holding, collision_free
+
+def main() -> None:
+    object_to_grasp = "beam_1"
+    print("Starting main")
+    rospy.init_node("test_beam")
+    print("Node initialized")
+    
+    print("Approach")
+    approach(object_to_grasp=object_to_grasp,
+             speed=1.0,
+             grasp="top", 
+             obstacle_clearance=0.15)
+    print(collision_free())
+    
+    print("Pick")
+    pick(object_to_grasp=object_to_grasp,
+         orientation=0., speed=.2,  obstacle_clearance=0.005, grasp_orientation="top")
+    time.sleep(4)
+    print(collision_free())
+    holding()
+    
+    print("Drop")
+    drop(object_to_grasp="sink",  orientation=0., speed=1., obstacle_clearance=0.02)
+    print(collision_free())
+
+    
+    rospy.signal_shutdown("Beam test finished")
+
+
+if __name__ == "__main__":
+    main()
