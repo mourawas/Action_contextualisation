d168369b9233e6d928988cb410abd94d31a8ff7a
mourawas <mouhamad.rawas@epfl.ch>
2025-09-29 16:08:36 +0200

    Trying to make the robot grasp the beam

--- ros_ws/src/primitive_library/scripts/test_beam.py
+++ ros_ws/src/primitive_library/scripts/test_beam.py
@@ -11,20 +11,20 @@ def main() -> None:
     
     print("Approach")
     approach(object_to_grasp=object_to_grasp,
-             speed=1.0,
+             speed=1.,
              grasp="top", 
              obstacle_clearance=0.15)
     print(collision_free())
     
     print("Pick")
     pick(object_to_grasp=object_to_grasp,
-         speed=.2, obstacle_clearance=0.005, grasp_orientation="top")
+         speed=0.1, obstacle_clearance=0.002, grasp_orientation="top")
     time.sleep(4)
     print(collision_free())
     holding()
     
     print("Drop")
-    drop(object_to_grasp="sink",  orientation=0., speed=1., obstacle_clearance=0.02)
+    drop(object_to_grasp="left_side",  orientation=0., speed=1., obstacle_clearance=0.02)
     print(collision_free())
 
     
